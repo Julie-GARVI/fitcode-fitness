4cac2592c9980faab9da5cf18e265e25ec9fc811
Julie-GARVI <julie.garvi@oclock.school>
2023-11-26 14:31:37 +0100

    comments

--- frontend/src/components/Comment.svelte
+++ frontend/src/components/Comment.svelte
@@ -1,6 +1,7 @@
 <script>
     export let title, rating, lastname, age, date, content;
     import endpoint from '../storage.js';
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
 
     let comments = [];
 
@@ -20,6 +21,7 @@
         }
     }
     getCommentData() 
+
 </script>
 
   <h2>Vos avis</h2>
@@ -30,37 +32,37 @@
 
         {#each comments as comment}
 
-          <div class="comment-container">
-
-            <div class="exercice-title">
-              <p>{comment.title}</p>
-            </div>
-    
-            <div class="star-comment">
-
-          <div class="rating">
-            <input value="5" name="rate" id="star5" type="radio">
-            <label title="text" for="star5"></label>
-            <input value="4" name="rate" id="star4" type="radio">
-            <label title="text" for="star4"></label>
-            <input value="3" name="rate" id="star3" type="radio" checked="">
-            <label title="text" for="star3"></label>
-            <input value="2" name="rate" id="star2" type="radio">
-            <label title="text" for="star2"></label>
-            <input value="1" name="rate" id="star1" type="radio">
-            <label title="text" for="star1"></label>
+            <div class="comment-container">
+  
+              <div class="exercice-title">
+                <p>{comment.title}</p>
+              </div>
+      
+              <div class="star-comment">
+  
+            <div class="rating">
+              <input value="5" name="rate" id="star5" type="radio">
+              <label title="text" for="star5"></label>
+              <input value="4" name="rate" id="star4" type="radio">
+              <label title="text" for="star4"></label>
+              <input value="3" name="rate" id="star3" type="radio" checked="">
+              <label title="text" for="star3"></label>
+              <input value="2" name="rate" id="star2" type="radio">
+              <label title="text" for="star2"></label>
+              <input value="1" name="rate" id="star1" type="radio">
+              <label title="text" for="star1"></label>
+          </div>
         </div>
+      
+            <div class="comment-user">
+              <p>Par {comment.user.firstname}, {comment.user.age} ans, le {comment.date} :</p>
+            </div>
+      
+            <div class="comment-content">
+              <p>{comment.content}</p>
+            </div>
       </div>
-    
-          <div class="comment-user">
-            <p>Par {comment.user.firstname} {comment.user.age} ans, le {comment.date} :</p>
-          </div>
-    
-          <div class="comment-content">
-            <p>{comment.content}</p>
-          </div>
-    </div>
-
+  
         {/each}
 
     <i class="fa-solid fa-arrow-right fa-xl"></i>
